0bcf0d9132ce247801912025504b8964ed1ded8b
IpPEi-code <get.on.natural.shb@gmail.com>
2020-06-21 11:21:16 +0900

    アロー関数を使ってみよう
    ドットJS24 アロー関数を使ってみようを受講したから

--- js/main.js
+++ js/main.js
@@ -1,67 +1,15 @@
 'use strict';
 
-// function sum(a, b, c) {
+// const sum = function(a, b, c) {
 //   return a + b + c;
-// }
+// };
+// const sum = (a, b, c) => a + b + c;
 
-const sum = function(a, b, c) {
-  return a + b + c;
-};
+// const total = sum(1, 2, 3) + sum(3, 4, 5);
+// console.log(total);
 
-
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-console.log(total);
-
-/* 関数宣言と関数式
-
-関数宣言(関数の定義)
-
-function 関数名(仮引数, 仮引数, ・・・) {
-  処理;
-  処理;
-  return 返り値;←必要があれば
-}
-
------
-呼び出し
-
-関数名(実引数, 実引数, ・・・);
-
----
-例)
-
-function sum(a, b, c) {
-  console.log(a + b + c);
-  return a + b + c;
-}
-
-const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
-console.log(total);
-
----
-
-関数式
-
-const 定数名 = function(仮引数, 仮引数, ・・・) {
-  処理;
-  処理;
-  return 返り値;
-};
-
----
-呼び出し
-
-定数名(実引数, 実引数, ・・・);
-
-無名関数
-
-例)
-const sum = function(a, b, c) {
-  return a + b + c;
-};
-
-
-const total = sum(1, 2, 3) + sum(3, 4, 5);
-console.log(total);
-
-*/
+// const double = function(a) {
+//   return a * 2;
+// };
+const double = a => a * 2;
+console.log(double(12));
